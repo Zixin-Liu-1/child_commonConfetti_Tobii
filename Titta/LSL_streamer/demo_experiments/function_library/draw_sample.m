11075ffe737a4200001f172b49974f4dd403dcbb
Zixin-Liu-1 <zixin.liu@uni-hamburg.de>
2025-01-30 15:49:19 +0100

    Moved Titta and TobiiPro to local

--- Titta/LSL_streamer/demo_experiments/function_library/draw_sample.m
+++
@@ -1,3 +0,0 @@
-function draw_sample(wpnt, x, y, gazeClr, gazeRadius)
-rect = CenterRectOnPointd([0 0 2 2]*gazeRadius,x,y);
-Screen('FrameOval', wpnt, gazeClr, rect, 5);
--- Titta/LSL_streamer/demo_experiments/function_library/tell_to_disconnect.m
+++
@@ -1,2 +0,0 @@
-function tell_to_disconnect(outlet,hostname)
-outlet.push_sample({sprintf('disconnect_stream,%s',hostname)});
